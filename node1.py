d595f5e28aa0fa323729c006bbb27146739510b3
Tim Herrmann <therrma2@gmail.com>
2015-09-18 14:18:20 -0500

    commit 3: fully working track

--- node1.py
+++ node1.py
@@ -6,10 +6,104 @@ import roslib
 from std_msgs.msg import String
 import sys
 import cv2
-import cv2.cv as cv
+#import cv2.cv as cv
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
+from std_msgs.msg import UInt16
+
+
+def node1cb(data, extra_args):
+    bridge,pub = extra_args
+    #rospy.loginfo("new image")
+
+    try:
+       frame1 = bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError, e:
+        print("==[CAMERA MANAGER]==", e)
+
+    hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
+    
+
+    #blur = cv2.GaussianBlur(frame1,(15,15),0)
+   
+    hmin = cv2.getTrackbarPos('HMin','filter')
+    smin = cv2.getTrackbarPos('SMin','filter')
+    vmin = cv2.getTrackbarPos('VMin','filter')
+
+    hmax = cv2.getTrackbarPos('HMax','filter')
+    smax = cv2.getTrackbarPos('SMax','filter')
+    vmax = cv2.getTrackbarPos('VMax','filter')
+
+    min_red_vales = np.array([hmin,smin,vmin])
+    max_red_vales = np.array([hmax,smax,vmax])
+
+    mask = cv2.inRange(hsv, min_red_vales, max_red_vales)
+    kernel = np.ones((380,380),np.uint8)
+    erosion = cv2.erode(mask,kernel,iterations = 1)#Erode
+    kernel1 = np.ones((380,380),np.uint8)
+    dilation = cv2.dilate(erosion,kernel1,iterations = 1)#Dilate
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+    #print len(cnts)
+
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        try:
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        except ZeroDivisonError:
+            return
+
+        
+        #print "first if"
+        if radius > 20:
+            #print "Radius =",radius
+            #print 'second if'
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(mask, (int(x), int(y)), int(radius),(0, 255, 255), 5 )
+            cv2.circle(mask, center, 5, (0, 0, 255), -1)
+
+        print(x,y)
+
+        x_int = int(x)
+        y_int = int(y)
+
+            
+        
+
+    
+    cv2.imshow('cv_image',mask)
+
+
+
+
+    #img2 = cv2.findContours(dilation,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #cv2.drawContours(frame1, img2, -1 (0,255,0), 3)
+    
+    #M= cv2.moments(dilation)
+    #centroid_x = int(M['m10']/M['m00'])
+    #centroid_y = int(M['m01']/M['m00'])
+
+    #cv2.circle(dilation,(centroid_x,centroid_y),5,(0,0,255),-1)
+
+    #circles = cv2.HoughCircles(dilation,cv2.cv.CV_HOUGH_GRADIENT,1,2 0,param1=50,param2=30,minRadius = 0, maxRadius=0)
+    #circles = np.uint16(np.around(circles))
+
+    #for i in circles[0,:]:
+        #cv2.circle(dilation,(i[0],i[1]),i[2],(0,255,0),2)
+        #cv2.circle(dilation,(i[0],i[1]),2,(0,0,255),3)
+
+    cv2.imshow('frame1',frame1)
+    cv2.imshow('dilation',mask)
+    
+    cv2.waitKey(25)
+
+def nothing(self):
+    pass
 
 
 def node1cb(data, bridge ):
@@ -41,14 +135,63 @@ def node1cb(data, bridge ):
     erosion = cv2.erode(mask,kernel,iterations = 1)#Erode
     kernel1 = np.ones((380,380),np.uint8)
     dilation = cv2.dilate(erosion,kernel1,iterations = 1)#Dilate
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+    #print len(cnts)
+
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        try:
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        except ZeroDivisonError:
+            return
+
+        
+        #print "first if"
+        if radius > 20:
+            #print "Radius =",radius
+            #print 'second if'
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(mask, (int(x), int(y)), int(radius),(0, 255, 255), 5 )
+            cv2.circle(mask, center, 5, (0, 0, 255), -1)
+
+        print(x,y)
+
+        x_int = int(x)
+        y_int = int(y)
+        msg = UInt16()
+        msg.data = x_int
+        pub.publish(msg)    
+        
+
     
-    circles = cv2.HoughCircles(dilation,cv.CV_HOUGH_GRADIENT,1,2 0,param1=50,param2=30,minRadius = 0, maxRadius=0)
-    circles = np.uint16(np.around(circles))
+    cv2.imshow('cv_image',mask)
 
-    for i in circles[0,:]:
-        cv2.circle(dilation,(i[0],i[1]),i[2],(0,255,0),2)
-        cv2.circle(dilation,(i[0],i[1]),2,(0,0,255),3)
 
+
+
+    #img2 = cv2.findContours(dilation,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #cv2.drawContours(frame1, img2, -1 (0,255,0), 3)
+    
+    #M= cv2.moments(dilation)
+    #centroid_x = int(M['m10']/M['m00'])
+    #centroid_y = int(M['m01']/M['m00'])
+
+    #cv2.circle(dilation,(centroid_x,centroid_y),5,(0,0,255),-1)
+
+    #circles = cv2.HoughCircles(dilation,cv2.cv.CV_HOUGH_GRADIENT,1,2 0,param1=50,param2=30,minRadius = 0, maxRadius=0)
+    #circles = np.uint16(np.around(circles))
+
+    #for i in circles[0,:]:
+        #cv2.circle(dilation,(i[0],i[1]),i[2],(0,255,0),2)
+        #cv2.circle(dilation,(i[0],i[1]),2,(0,0,255),3)
+
+
+        
     cv2.imshow('frame1',frame1)
     cv2.imshow('dilation',mask)
     
@@ -66,16 +209,18 @@ def node1():
 
     cv2.namedWindow ('filter')
 
-    cv2.createTrackbar('HMin','filter',2,255,nothing)
-    cv2.createTrackbar('SMin','filter',102,255,nothing)
-    cv2.createTrackbar('VMin','filter',198,255,nothing)
+    cv2.createTrackbar('HMin','filter',22,255,nothing)
+    cv2.createTrackbar('SMin','filter',78,255,nothing)
+    cv2.createTrackbar('VMin','filter',132,255,nothing)
 
-    cv2.createTrackbar('HMax','filter',20,255,nothing)
-    cv2.createTrackbar('SMax','filter',155,255,nothing)
-    cv2.createTrackbar('VMax','filter',232,255,nothing)
+    cv2.createTrackbar('HMax','filter',200,255,nothing)
+    cv2.createTrackbar('SMax','filter',194,255,nothing)
+    cv2.createTrackbar('VMax','filter',228,255,nothing)
 
 
-    rospy.Subscriber('usb_cam/image_raw', Image, node1cb,bridge)
+    pub = rospy.Publisher('node1pub', UInt16, queue_size=10)
+    rospy.Subscriber('usb_cam/image_raw', Image, node1cb, callback_args=(bridge, pub))
+
     
     rospy.spin()
 
--- node2.py
+++ node2.py
@@ -10,29 +10,51 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import serial
+from std_msgs.msg import UInt16
 
+pantarget = 1500
+tilttarget = 1500
 
+def node2cb(data, extra_args):
 
+    ser,var = extra_args
+    x = data.data
+    print x
 
-def node2cb(data):
+    global pantarget
+    global tilttarget
+
+    if var == 'x':
+        if x >= 340 and pantarget < 1950:
+            #increase pan target by 
+            pantarget += 10
+        elif x <= 300 and pantarget > 1050:
+            pantarget -= 10
+    if var == 'y':
+        if x >= 260 and tilttarget < 1950:
+            #increase pan target by 
+            tilttarget -= 10
+        elif x <= 220 and tilttarget > 1050:
+            tilttarget += 10
+            
     
     #tiltpos = bytearray([132,0,x,x])    
     #panpos = bytearray([132,1,x,x])
 
-    paninput = int(raw_input("enter pan value> "))
-    tiltinput = int(raw_input("enter tilt value> "))
+    #paninput = int(x)
+    #tiltinput = int(raw_input("enter tilt value> "))
 
-    pancom = paninput*4 & 0x7f
-    pancom2 = (paninput*4 >> 7) & 0x7f
+    pancom = pantarget*4 & 0x7f
+    pancom2 = (pantarget*4 >> 7) & 0x7f
 
-    tiltcom = tiltinput*4 & 0x7f
-    tiltcom2 = (tiltinput*4 >>7) & 0x7f
+    tiltcom = tilttarget*4 & 0x7f
+    tiltcom2 = (tilttarget*4 >>7) & 0x7f
 
     #print pancom, pancom2
     #print tiltcom, tiltcom2
 
-    panpos = bytearray([132,1,pancom,pancom2])
-    tiltpos = bytearray([132,0,tiltcom,tiltcom2])
+    panpos = bytearray([132,0,pancom,pancom2])
+    tiltpos = bytearray([132,1,tiltcom,tiltcom2])
 
     ser.write(panpos)
     ser.write(tiltpos)
@@ -45,7 +67,14 @@ def node2():
 
     ser = serial.Serial('/dev/ttyACM0')
 
-    rospy.Subscriber('node1', Image, node2cb)
+    initialpan = bytearray([132,1,112,46])
+    initialtilt = bytearray([132,0,112,46])
+
+    ser.write(initialpan)
+    ser.write(initialtilt)
+    
+    rospy.Subscriber('node1pub', UInt16, node2cb,(ser,"x"))
+    rospy.Subscriber('node1pub2', UInt16, node2cb,(ser,"y"))
     
     rospy.spin()
 
